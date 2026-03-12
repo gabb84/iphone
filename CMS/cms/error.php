2819b0cf2f4c4bc502133311d690c920e8d55640
gabb84 <bondocgj04@gmail.com>
2026-03-12 13:10:42 +0800

    Add files via upload

--- CMS/cms/error.php
+++ CMS/cms/error.php
@@ -1,29 +1,19 @@
 <!DOCTYPE html>
 <html>
+    <head>
+        <meta charset="UTF-8">
+        <title>An error occurred</title>
+        <link rel="stylesheet" type="text/css" href="css/styles.css">
+    </head>
+    <body>
 
-<head>
-<meta charset="UTF-8">
-<title>An error occurred</title>
-<link rel="stylesheet" type="text/css" href="css/styles.css">
-</head>
+        <h1 class="logo">Creative folk</h1>
 
-<body>
+        <h1>Sorry! An error occurred.</h1>
+        <p>The site owners have been notified and will fix the problem as soon as possible.</p>
+        <p><a href="index.php">Click here to go back to the home page</a></p>
+        <p>If the problem persists <a href="mailto:hello@eg.link">click here to email us</a>.
+        </p>
 
-<h1 class="logo">Creative folk</h1>
-
-<h1>Sorry! An error occurred.</h1>
-
-<p>The site owners have been notified and will fix the problem as soon as possible.</p>
-
-<p>
-<a href="index.php">Click here to go back to the home page</a>
-</p>
-
-<p>
-If the problem persists
-<a href="mailto:hello@eg.link">click here to email us</a>.
-</p>
-
-</body>
-
+    </body>
 </html>
